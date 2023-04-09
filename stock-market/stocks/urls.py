5ea6cdeadb751fb53c2641f29689c7d9a84e1777
Sreemanti Dey <sreemantidey1234@gmail.com>
2023-04-09 06:41:33 +0530

    template and views queries working:

--- stock-market/stocks/urls.py
+++ stock-market/stocks/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.stocksview, name='stocksview'),
+    path('stocks/', views.stocksall, name='stocksall'),
 ]
--- stock-market/stocks/views.py
+++ stock-market/stocks/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import StockMetadata
 def stocksview(request):
     return HttpResponse("Hello, Views to be seen here!")
+
+def stocksall(request):
+    stocks = StockMetadata.objects.all()
+    return render (request, 'stock_details.html', {'stocks': stocks[0]})
