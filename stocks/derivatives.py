83f5a5ce0cdb43ec7b19db12dbb1095062958b23
Sreemanti Dey <77806432+alexriderspy@users.noreply.github.com>
2023-04-23 16:51:14 +0530

    options and thread for derivatives added

--- stocks/derivatives.py
+++ stocks/derivatives.py
@@ -14,11 +14,23 @@ def derivatives(buyer, seller, stock_id, num_shares, price_per_share,premium, ex
     with connection.cursor() as cursor:
         cursor.execute(query_togetid)
         val = dictfetchall(cursor)
-        print(val[0]['count'])
+        id = val[0]['count']
 
-        query = '''insert into derivatives values(%s,%s,%s,%s,%s,%s,%s,%s,%s, %s, %s)'''
-        params = [val[0]['count'], buyer, seller,stock_id,today,num_shares,price_per_share,False,'2024-05-09',premium,'options']
+    query = '''select id from customer where name = %s;'''
+    with connection.cursor() as cursor:
+        cursor.execute(query, [buyer])
+        val = dictfetchall(cursor)
+        buyer_id = val[0]['id']
     
+    with connection.cursor() as cursor:
+        cursor.execute(query, [seller])
+        val = dictfetchall(cursor)
+        seller_id = val[0]['id']
+
+    query = '''insert into derivatives values(%s,%s,%s,%s,%s,%s,%s,%s,%s, %s, %s)'''
+    params = [id, buyer_id, seller_id,stock_id,today,num_shares,price_per_share,False,'2024-05-09',premium,'options']
+
+
     with connection.cursor() as cursor:
         cursor.execute(query,params)
 
